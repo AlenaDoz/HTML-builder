84d375aa74e5b7d10a938c71a2c1be12e365bf97
AlenaDoz <alena.abunassar1@mail.ru>
2022-05-19 11:37:36 +0300

    feat: add 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
 
+const path = require('path');
+const fsPromises = require('fs/promises');
+async function findFiles(searchPath) {
+    let files = await fsPromises.readdir(path.join(__dirname, ...searchPath), { withFileTypes: true });
+    let nFiles = [];
+    for (let file of files) {
+        if (file.isFile()) {
+            file.path = searchPath;
+            nFiles.push(file);
+        }
+    }
+    return nFiles;
+}
+async function copyMaker() {
+    await fsPromises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+    let files = await findFiles(['files']);
+    files.forEach(async file => {
+        await fsPromises.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name));
+    });
+    console.log(files);
+}
+copyMaker();
